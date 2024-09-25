fb1fd99fa97daee7b03ac29d4c019e9f19112692
AlexRunch <4mihailov@gmail.com>
2024-09-25 11:39:36 +0200

    Update bind_function.py

--- bind_function.py
+++ bind_function.py
@@ -191,80 +191,53 @@ def save_channel_to_dynamodb(channel_id, admin_user_id, subscribers, email=None,
 async def process_message(client, chat_id, text, user_id, user_name):
     logger.info(f"Получено сообщение: {text} от пользователя {user_name} (ID: {user_id})")
     
+    try:
+        response = USER_TABLE.get_item(
+            Key={'user_id': str(user_id)}
+        )
+        if 'Item' in response:
+            last_message = response['Item'].get('last_message', '')
+            if last_message == text:
+                logger.info(f"Пользователь {user_name} (ID: {user_id}) уже получил это сообщение: {text}")
+                return
+    except Exception as e:
+        logger.error(f"Ошибка при получении последнего сообщения пользователя {user_id} из DynamoDB: {str(e)}")
+        raise
+
     if text == '/start':
-        user_state = await get_user_state(user_id)
-        if user_state != 'started':
-            logger.info("Обработка команды /start")
-            welcome_message = ("Привет! Я бот для отслеживания изменений подписчиков вашего канала.\n\n"
-                               "Чтобы подключить канал, выполните следующие шаги:\n"
-                               "1. Добавьте меня в качестве администратора в ваш канал\n"
-                               "2. Напишите мне @username вашего канала\n"
-                               "3. После успешной проверки, напишите свою электронную почту\n\n"
-                               "Инструкция по добавлению бота в канал:\n\n"
-                               "Вариант №1. Через настройки канала:\n"
-                               "1. Зайдите в настройки вашего канала.\n"
-                               "2. Перейдите в раздел Администраторы.\n"
-                               "3. Нажмите Добавить администратора.\n"
-                               "4. В поле поиска введите название бота: @mysubsinoutbot.\n"
-                               "5. Для безопасности можете отключить все разрешения для этого администратора.\n"
-                               "6. Нажмите Готово для завершения.\n\n"
-                               "Вариант №2. Через интерфейс бота:\n"
-                               "1. Откройте чат с ботом @mysubsinoutbot.\n"
-                               "2. Нажмите на имя бота в верхней части экрана.\n"
-                               "3. На открывшейся странице с информацией о боте выберите опцию Добавить в группу или канал.\n"
-                               "4. Выберите название своего канала из списка.\n"
-                               "5. Убедитесь, что включены права администратора.\n"
-                               "6. Подтвердите добавление бота.\n\n"
-                               "Следуя этой инструкции, вы успешно добавите бота в свой канал!\n\n"
-                               "По всем вопросам обращайтесь к @alex_favin")
-            await send_message(client, chat_id, welcome_message)
-            await save_user_to_dynamodb(user_id, user_name, text, state='started', admin_username=user_name)
-        else:
-            logger.info(f"Пользователь {user_name} (ID: {user_id}) уже получил сообщение /start")
+        logger.info("Обработка команды /start")
+        welcome_message = ("Привет! Я бот для отслеживания изменений подписчиков вашего канала.\n\n"
+                           "Чтобы подключить канал, выполните следующие шаги:\n"
+                           "1. Добавьте меня в качестве администратора в ваш канал\n"
+                           "2. Напишите мне @username вашего канала\n"
+                           "3. После успешной проверки, напишите свою электронную почту\n\n"
+                           "Инструкция по добавлению бота в канал:\n\n"
+                           "Вариант №1. Через настройки канала:\n"
+                           "1. Зайдите в настройки вашего канала.\n"
+                           "2. Перейдите в раздел Администраторы.\n"
+                           "3. Нажмите Добавить администратора.\n"
+                           "4. В поле поиска введите название бота: @mysubsinoutbot.\n"
+                           "5. Для безопасности можете отключить все разрешения для этого администратора.\n"
+                           "6. Нажмите Готово для завершения.\n\n"
+                           "Вариант №2. Через интерфейс бота:\n"
+                           "1. Откройте чат с ботом @mysubsinoutbot.\n"
+                           "2. Нажмите на имя бота в верхней части экрана.\n"
+                           "3. На открывшейся странице с информацией о боте выберите опцию Добавить в группу или канал.\n"
+                           "4. Выберите название своего канала из списка.\n"
+                           "5. Убедитесь, что включены права администратора.\n"
+                           "6. Подтвердите добавление бота.\n\n"
+                           "Следуя этой инструкции, вы успешно добавите бота в свой канал!\n\n"
+                           "По всем вопросам обращайтесь к @alex_favin")
+        await send_message(client, chat_id, welcome_message)
+        await save_user_to_dynamodb(user_id, user_name, text, state='started', admin_username=user_name)
     elif text == '/stop':
         logger.info("Обработка команды /stop")
         await send_message(client, chat_id, "Бот остановлен. Для возобновления работы используйте /start")
         await save_user_to_dynamodb(user_id, user_name, text, state='stopped', admin_username=user_name)
-    elif text.startswith('@'):
-        channel_name = text
-        is_admin = await verify_channel_admin(client, user_id, channel_name)
-        if is_admin:
-            await send_message(client, chat_id, "Канал успешно проверен. Пожалуйста, напишите вашу электронную почту.")
-            try:
-                subscribers = await get_subscribers_list(client, channel_name)
-                save_channel_to_dynamodb(channel_name, user_id, subscribers, admin_name=user_name)
-                logger.info(f"Канал {channel_name} успешно сохранен в DynamoDB для пользователя {user_id}")
-            except Exception as e:
-                logger.error(f"Ошибка при сохранении канала {channel_name} в DynamoDB для пользователя {user_id}: {str(e)}")
-                await send_message(client, chat_id, "Произошла ошибка при сохранении данных канала. Пожалуйста, попробуйте еще раз.")
-        else:
-            await send_message(client, chat_id, "Вы не являетесь администратором этого канала или бот не добавлен в администраторы. Пожалуйста, проверьте и попробуйте снова.")
-    elif '@' in text and '.' in text:  # Простая проверка на email
-        email = text
-        try:
-            channel_name = get_channel_from_dynamodb(user_id)
-            logger.info(f"Получено название канала из DynamoDB для пользователя {user_id}: {channel_name}")
-        except Exception as e:
-            logger.error(f"Ошибка при получении названия канала из DynamoDB для пользователя {user_id}: {str(e)}")
-            channel_name = None
-        
-        if channel_name:
-            try:
-                subscribers = await get_subscribers_list(client, channel_name)
-                logger.info(f"Получен список подписчиков для канала {channel_name}")
-                send_email(channel_name, email, len(subscribers), json.dumps(subscribers, ensure_ascii=False, indent=2))
-                logger.info(f"Отправлено email на адрес {email} с информацией о канале {channel_name}")
-                await send_message(client, chat_id, f"Канал {channel_name} успешно подключен! Информация отправлена на {email}")
-                save_channel_to_dynamodb(channel_name, user_id, subscribers, email, admin_name=user_name)
-            except Exception as e:
-                logger.error(f"Ошибка при обработке email {email} для канала {channel_name}: {str(e)}")
-                await send_message(client, chat_id, "Произошла ошибка при обработке вашего запроса. Пожалуйста, попробуйте еще раз.")
-        else:
-            logger.warning(f"Не удалось найти канал в DynamoDB для пользователя {user_id}")
-            await send_message(client, chat_id, "Произошла ошибка. Пожалуйста, начните процесс подключения канала заново с команды /start")
     else:
-        await send_message(client, chat_id, "Я не понимаю эту команду. Пожалуйста, следуйте инструкциям или используйте /start для начала.")
+        # Обновление времени последнего взаимодействия для других сообщений
         await save_user_to_dynamodb(user_id, user_name, text, state='active', admin_username=user_name)
+    # ... остальной код ...
 
 def get_channel_from_dynamodb(admin_user_id):
     try:
@@ -353,7 +326,7 @@ async def save_user_to_dynamodb(user_id, user_name, text, state='', admin_userna
                 'last_message': text,
                 'state': state,
                 'admin_username': admin_username,
-                'timestamp': int(time.time()),
+                'timestamp': int(time.time()),  # Сохранение текущего времени в секундах
                 'last_interaction': int(time.time())  # Сохранение времени последнего взаимодействия
             }
         )
